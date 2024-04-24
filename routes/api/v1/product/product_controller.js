ec5b78b67f16d9f72481da176c82fc5f7f2d34b8
kimssangman <136281131+kimssangman@users.noreply.github.com>
2024-04-24 19:03:14 +0900

    스크롤에 따른 커스텀 hook 사용 완료

--- routes/api/v1/product/product_controller.js
+++ routes/api/v1/product/product_controller.js
@@ -8,11 +8,19 @@ exports.getProduct = async (req, res) => {
   router.get('product', productController.product) 
 --------------------------------------------------`);
     const dbModels = global.DB_MODELS;
+    const page = parseInt(req.query.page); // 페이지 번호 가져오기
+
+    console.log(page);
 
     try {
-        const product = await dbModels.Product.find();
+        const productsPerPage = 5; // 페이지당 상품 수
+        const skipCount = (page - 1) * productsPerPage; // 건너뛸 상품 수
 
-        res.status(201).send(product);
+        const products = await dbModels.Product.find()
+            .skip(skipCount) // 건널뛸 문서 수
+            .limit(productsPerPage); // 반환할 문서의 최대 수
+
+        res.status(201).send(products);
     } catch (error) {
         console.log(error);
         return res.status(500).send({
